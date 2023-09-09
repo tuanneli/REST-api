ac5711e2902b972318c03c73d831123b32f4b713
tuanneli <zhuravlev99pic@gmail.com>
2023-09-09 18:04:55 +0300

    setup pg db

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	todo "github.com/tuanneli/REST-api.git"
 	"github.com/tuanneli/REST-api.git/pkg/handler"
 	"github.com/tuanneli/REST-api.git/pkg/repository"
 	"github.com/tuanneli/REST-api.git/pkg/service"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,11 +18,29 @@ func main() {
 		log.Fatalf("Error initializing config: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("faild initializing db: %s", err.Error())
+	}
+
+	port := viper.GetString("port")
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	err := srv.Run(viper.GetString("port"), handlers.InitRoutes())
+	fmt.Println("PORT:", port)
+	err = srv.Run(port, handlers.InitRoutes())
 
 	if err != nil {
 		log.Fatalf("An error occurred: %s", err.Error())
@@ -26,8 +48,6 @@ func main() {
 }
 
 func initConfig() error {
-	viper.SetConfigFile("configs")
-	viper.SetConfigFile("config")
-	viper.SetConfigType("json")
+	viper.SetConfigFile("./configs/config.json")
 	return viper.ReadInConfig()
 }
